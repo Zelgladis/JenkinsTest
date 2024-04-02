650d5fbfd04cc08f8c1715b12d7e30247ef4bc0d
zelgladis <Cep908@gmail.com>
2024-04-02 12:11:10 +0400

    fix

--- vars/run_pipe.groovy
+++ vars/run_pipe.groovy
@@ -1,9 +1,9 @@
 // vars/run.pipe.groovy
 
-def call(){
+def call(String br){
     try{
         mpline.init_env()
-        mpline.step1(BRANCH_NAME)
+        mpline.step1(br)
         //mpline.failed_step()
         mpline.step_print()
     }catch(Exception e){
