6954994b8c8268523aa088349c80f74f7dd0a563
zelgladis <Cep908@gmail.com>
2024-04-02 10:55:06 +0400

    run pipe aded

---
+++ vars/run_pipe.groovy
@@ -0,0 +1,14 @@
+// vars/run.pipe.groovy
+
+try{
+    mpline.init_env()
+    mpline.step1(BRANCH_NAME)
+    //mpline.failed_step()
+    mpline.step_print()
+}catch(Exception e){
+    throw new Exception("Something went wrong!")
+    
+}finally{
+    mpline.clean()
+}
+
