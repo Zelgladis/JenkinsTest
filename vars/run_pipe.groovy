5d0ea5260efbbdb70eda313c1fe95ff901ac7833
zelgladis <Cep908@gmail.com>
2024-04-02 10:56:47 +0400

    def call

--- vars/run_pipe.groovy
+++ vars/run_pipe.groovy
@@ -1,14 +1,15 @@
 // vars/run.pipe.groovy
 
-try{
-    mpline.init_env()
-    mpline.step1(BRANCH_NAME)
-    //mpline.failed_step()
-    mpline.step_print()
-}catch(Exception e){
-    throw new Exception("Something went wrong!")
-    
-}finally{
-    mpline.clean()
+def call(){
+    try{
+        mpline.init_env()
+        mpline.step1(BRANCH_NAME)
+        //mpline.failed_step()
+        mpline.step_print()
+    }catch(Exception e){
+        throw new Exception("Something went wrong!")
+        
+    }finally{
+        mpline.clean()
+    }
 }
-
