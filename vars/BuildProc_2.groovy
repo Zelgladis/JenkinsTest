b218243c087dbfaea88891b56e345f415a1bd6a6
zelgladis <Cep908@gmail.com>
2024-05-30 12:48:50 +0400

    zxc

--- vars/BuildProc_2.groovy
+++ vars/BuildProc_2.groovy
@@ -36,7 +36,8 @@ pipelineJob(yamlData.pipelines[**c**].name) {
                 "${(yamlData.pipelines[**c**].parameters.ProjectPath ? "${spaces}ProjectPath: '${yamlData.pipelines[**c**].parameters.ProjectPath}'," : "")}" + \
                 "${(yamlData.pipelines[**c**].parameters.k8s_namespace ? "${spaces}k8s_namespace: '${yamlData.pipelines[**c**].parameters.k8s_namespace}'," : "")}" + \
                 "${(yamlData.pipelines[**c**].parameters.k8s_cluster_api ? "${spaces}k8s_cluster_api: '${yamlData.pipelines[**c**].parameters.k8s_cluster_api}'," : "")}" + \
-                "${(yamlData.pipelines[**c**].parameters.server_name ? "${spaces}server_name: '${yamlData.pipelines[**c**].parameters.server_name}'," : "")}"
+                "${(yamlData.pipelines[**c**].parameters.server_name ? "${spaces}server_name: '${yamlData.pipelines[**c**].parameters.server_name}'," : "")}" + \
+                "${(yamlData.pipelines[**c**].parameters.standServiceDir ? "${spaces}standServiceDir: '${yamlData.pipelines[**c**].parameters.standServiceDir}'," : "")}"
             // Получаем красивый паплайн
             def scriptContent = "@Library('lab') _\n" + \
                 "RunPipelineX(" + \
