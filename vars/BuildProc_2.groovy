fb18c5c4e8bd5d92d3d88e8e6ffbb1a09645e312
zelgladis <Cep908@gmail.com>
2024-05-30 12:52:51 +0400

    dfghj

--- vars/BuildProc_2.groovy
+++ vars/BuildProc_2.groovy
@@ -40,7 +40,8 @@ pipelineJob(yamlData.pipelines[**c**].name) {
                 "${(yamlData.pipelines[**c**].parameters.standServiceDir ? "${spaces}standServiceDir: '${yamlData.pipelines[**c**].parameters.standServiceDir}'," : "")}" + \
                 "${(yamlData.pipelines[**c**].parameters.DotNetDockerPath ? "${spaces}DotNetDockerPath: '${yamlData.pipelines[**c**].parameters.DotNetDockerPath}'," : "")}"  + \
                 "${(yamlData.pipelines[**c**].parameters.mvntest ? "${spaces}mvntest: '${yamlData.pipelines[**c**].parameters.mvntest}'," : "")}" + \
-                "${(yamlData.pipelines[**c**].parameters.service_path ? "${spaces}service_path: '${yamlData.pipelines[**c**].parameters.service_path}'," : "")}"
+                "${(yamlData.pipelines[**c**].parameters.service_path ? "${spaces}service_path: '${yamlData.pipelines[**c**].parameters.service_path}'," : "")}" + \
+                "${(yamlData.pipelines[**c**].parameters.pom_path ? "${spaces}pom_path: '${yamlData.pipelines[**c**].parameters.pom_path}'," : "")}"
             // Получаем красивый паплайн
             def scriptContent = "@Library('lab') _\n" + \
                 "RunPipelineX(" + \
