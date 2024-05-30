130574178c6af5bf55524e3347ed2c043357de48
zelgladis <Cep908@gmail.com>
2024-05-30 13:55:18 +0400

    sdf

--- vars/RunPipelineX.groovy
+++ vars/RunPipelineX.groovy
@@ -1,5 +1,22 @@
 def call(Map args){
     node(){
+        stage('Checkout') {
+            steps {
+                script {
+                    // Использование параметра GIT_BRANCH для клонирования репозитория
+                    checkout([$class: 'GitSCM',
+                              branches: [[name: "${params.GIT_BRANCH}"]],
+                              doGenerateSubmoduleConfigurations: false,
+                              extensions: [],
+                              submoduleCfg: [],
+                              userRemoteConfigs: [[
+                                  url: 'ssh://git@github.com/Zelgladis/JenkinsTest.git',
+                                  credentialsId: "${env.MY_GIT}"
+                              ]]
+                    ])
+                }
+            }
+        }
         stage('RunPipeX'){
             args.each { arg ->
                 echo "$arg"
