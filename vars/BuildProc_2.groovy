187fdbef417f6535a0e495ac016ed2a93a2d569c
zelgladis <Cep908@gmail.com>
2024-05-30 12:50:01 +0400

    cvbn

--- vars/BuildProc_2.groovy
+++ vars/BuildProc_2.groovy
@@ -37,7 +37,8 @@ pipelineJob(yamlData.pipelines[**c**].name) {
                 "${(yamlData.pipelines[**c**].parameters.k8s_namespace ? "${spaces}k8s_namespace: '${yamlData.pipelines[**c**].parameters.k8s_namespace}'," : "")}" + \
                 "${(yamlData.pipelines[**c**].parameters.k8s_cluster_api ? "${spaces}k8s_cluster_api: '${yamlData.pipelines[**c**].parameters.k8s_cluster_api}'," : "")}" + \
                 "${(yamlData.pipelines[**c**].parameters.server_name ? "${spaces}server_name: '${yamlData.pipelines[**c**].parameters.server_name}'," : "")}" + \
-                "${(yamlData.pipelines[**c**].parameters.standServiceDir ? "${spaces}standServiceDir: '${yamlData.pipelines[**c**].parameters.standServiceDir}'," : "")}"
+                "${(yamlData.pipelines[**c**].parameters.standServiceDir ? "${spaces}standServiceDir: '${yamlData.pipelines[**c**].parameters.standServiceDir}'," : "")}" + \
+                "${(yamlData.pipelines[**c**].parameters.DotNetDockerPath ? "${spaces}DotNetDockerPath: '${yamlData.pipelines[**c**].parameters.DotNetDockerPath}'," : "")}" 
             // Получаем красивый паплайн
             def scriptContent = "@Library('lab') _\n" + \
                 "RunPipelineX(" + \
