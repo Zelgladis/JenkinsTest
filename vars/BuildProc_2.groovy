a893e4ae5e36ab752b719f20048ee2293083d6c2
zelgladis <Cep908@gmail.com>
2024-06-07 14:26:38 +0400

    asd

--- vars/BuildProc_2.groovy
+++ vars/BuildProc_2.groovy
@@ -13,7 +13,7 @@ pipelineJob(yamlData.pipelines[c].name) {
                 selectedValue('NONE')
                 useRepository("ssh://git@github.com/${yamlData.pipelines[c].parameters.globalSystem}/${yamlData.pipelines[c].parameters.GitName}.git")
             }
-            password(name: 'MY_SECRET', defaultValue: 'default_password', description: 'Enter your secret password')
+            passwordParam('MY_SECRET', 'default_password', 'Enter your secret password')
       if (yamlData.pipelines[c].parameters.mvncommand == 'dotnet nupkg') {
         stringParam('VersionNupkg', '2.0.', 'Версия пакетов')}
       else {
