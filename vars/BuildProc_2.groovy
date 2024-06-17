0fda9d32fb931dae2be2c4dfd5c5ae3d88e8c6e9
zelgladis <Cep908@gmail.com>
2024-06-17 23:00:16 +0400

    ijp

--- vars/BuildProc_2.groovy
+++ vars/BuildProc_2.groovy
@@ -1,5 +1,6 @@
 //p_val.add(Jenkins.instance.getItemByFullName(yamlData.pipelines[c].name)?.getProperty(hudson.model.ParametersDefinitionProperty)?.getParameterDefinition('PARAM_NAME')?.defaultValue)
 //p_val.add(job(yamlData.pipelines[c].name)?.getProperty(ParametersDefinitionProperty.class)?.getParameterDefinition('PARAM_NAME')?.defaultValue)
+p_val = binding.variables.PARAM_NAME.defaultValue
 
 pipelineJob(yamlData.pipelines[c].name) {
     parameters {
@@ -29,7 +30,7 @@ pipelineJob(yamlData.pipelines[c].name) {
             //binding.variables.each {
             //  println "${it.key} = ${it.value}"
             //}
-            println job(yamlData.pipelines[c].name).params.get('PARAM_NAME')
+            //println job(yamlData.pipelines[c].name).params.get('PARAM_NAME')
             if (p_val[c] == null) {
                 stringParam('PARAM_NAME', 'default_value', 'Description of the parameter')
             } else {
