26d684df3ac50aaa7235c15d4aef77f1f567b1bb
zelgladis <Cep908@gmail.com>
2024-06-18 18:27:59 +0400

    asd

--- vars/BuildProc_2.groovy
+++ vars/BuildProc_2.groovy
@@ -23,9 +23,19 @@ pipelineJob(yamlData.pipelines[c].name) {
             def job_name = 'creatertest/'+yamlData.pipelines[c].name
             def p_val = Jenkins.instance.getItemByFullName(job_name)?.getProperty(hudson.model.ParametersDefinitionProperty)?.getParameterDefinition('PARAM_NAME')?.defaultValue
             //println job(job_name).getProperty('Phudson.model.ParametersDefinitionProperty')?.getParameterDefinition('PARAM_NAME')?.defaultValue
-            def param = job(job_name).getProperty('hudson.model.ParametersDefinitionProperty')
+            def param = params()
             //def param = params.find { it.name == 'PARAM_NAME' }
-            println param
+            job.with {
+              parameters {
+                    def param = get(paramName)
+                    if (param instanceof hudson.model.StringParameterDefinition) {
+                        println "Parameter '$paramName' found in job '$jobName'"
+                        println "Default value: ${param.defaultValue}"
+                    } else {
+                        println "Parameter '$paramName' is not a StringParameterDefinition or does not exist in job '$jobName'"
+                    }
+                }
+            }
 
             if (p_val == null) {
                 stringParam('PARAM_NAME', 'default_value', 'Description of the parameter')
