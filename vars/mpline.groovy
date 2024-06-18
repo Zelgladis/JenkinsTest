51a9cffa8c84a4f4b97d90222054ea2a55311696
zelgladis <Cep908@gmail.com>
2024-06-18 10:39:28 +0400

    asd

--- vars/mpline.groovy
+++ vars/mpline.groovy
@@ -259,7 +259,7 @@ def rekurwa2(String stroka, List<String> folders=[], String prew=''){
 
 def rekurwa(String stroka, int i=0 ,List<String> res=[]){
     sp_str = stroka.split('/')
-    if(sp_str.size() != i-3){
+    if(i != sp_str.size()-1){
         res.add(sp_str[0..i].join('/'))
         return rekurwa(stroka, i+1, res)
     }else{
