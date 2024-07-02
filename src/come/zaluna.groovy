f6d5bc52dbefcd36c18f6442756f592906b7ef73
zelgladis <Cep908@gmail.com>
2024-07-02 13:35:44 +0400

    sdf

--- src/come/zaluna.groovy
+++ src/come/zaluna.groovy
@@ -1,12 +1,11 @@
 package come
 
-def asd = 'asd'
-def greenSt = '\u001B[32m'
-def redSt = '\u001B[31m'
-def yellowSt = '\u001B[32m'
-def closeCol = '\u001B[0m'
-
 def call(){
+    def asd = 'asd'
+    def greenSt = '\u001B[32m'
+    def redSt = '\u001B[31m'
+    def yellowSt = '\u001B[32m'
+    def closeCol = '\u001B[0m'
     ansiColor('xtrem'){
         println "${greenSt}Zdraste ERROR${closeCol}"
     }
