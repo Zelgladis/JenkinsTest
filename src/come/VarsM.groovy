1dfb11bd7b0014c70d517397a78ce4f6aeb97620
zelgladis <Cep908@gmail.com>
2024-07-02 13:39:55 +0400

    asd

---
+++ src/come/VarsM.groovy
@@ -0,0 +1,14 @@
+package come
+
+class VarsM {
+    static String asd = 'asd'
+    static String greenSt = '\u001B[32m'
+    static String redSt = '\u001B[31m'
+    static String yellowSt = '\u001B[32m'
+    static String closeCol = '\u001B[0m'
+    static String greeting = 'Hello World!'
+
+    static void VarsM() {
+        echo asd
+    }
+}
--- src/come/zaluna.groovy
+++ src/come/zaluna.groovy
@@ -1,12 +1,7 @@
 package come
 
 def call(){
-    def asd = 'asd'
-    def greenSt = '\u001B[32m'
-    def redSt = '\u001B[31m'
-    def yellowSt = '\u001B[32m'
-    def closeCol = '\u001B[0m'
     ansiColor('xtrem'){
-        println "${greenSt}Zdraste ERROR${closeCol}"
+        println "${VarsM.greenSt}Zdraste ERROR${VarsM.closeCol}"
     }
 }
