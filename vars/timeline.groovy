4279de8bdc6ccc63221fc5bf2609398f34ee6581
Mio <Cep908@gmail.com>
2024-11-05 10:59:04 +0300

    fix 3 test

--- vars/timeline.groovy
+++ vars/timeline.groovy
@@ -4,15 +4,7 @@ def call(){
                                                       passwordVariable: 'GIT_PASSWORD')]) {
         sh"""
             git clone https://\$GIT_USERNAME:\$GIT_PASSWORD@github.com/Zelgladis/JenkinsTest.git
+            ls
         """
-        def versions = []
-        def command = "cat ./JenkinsTest/testUsers.json"
-        def process = command.execute()
-        process.in.eachLine { line ->
-            // Обрабатываем JSON-ответ, чтобы извлечь версии
-            def history = new groovy.json.JsonSlurper().parseText(line)
-            versions = history.collect { it.revision }
-        }
-        echo "$versions"
     }
 }
