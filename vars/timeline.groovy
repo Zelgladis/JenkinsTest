bdf5c475675671f9b33fcc14f7ed4af60c04685a
Mio <Cep908@gmail.com>
2024-11-05 10:55:27 +0300

    fix 1

--- vars/timeline.groovy
+++ vars/timeline.groovy
@@ -1,7 +1,7 @@
 def call(){
-    withCredentials(usernamePassword[credentialsId: 'git_key',
-                                     usernameVariable: 'GIT_USERNAME',
-                                     passwordVariable: 'GIT_PASSWORD']) {
+    withCredentials([usernamePassword(credentialsId: 'git_key', 
+                                                      usernameVariable: 'GIT_USERNAME', 
+                                                      passwordVariable: 'GIT_PASSWORD')]) {
         sh"""
             git clone https://\$GIT_USERNAME:\$GIT_PASSWORD@github.com:Zelgladis/JenkinsTest.git
         """
