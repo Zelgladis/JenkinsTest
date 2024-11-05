af5f6f373e158177a61106c5468becb7ebf14d07
Mio <Cep908@gmail.com>
2024-11-05 10:53:07 +0300

    json parse

--- vars/timeline.groovy
+++ vars/timeline.groovy
@@ -1,5 +1,18 @@
 def call(){
-    echo "Nothing"
-    echo "Main_2 changesTEST"
-    echo "Merge test"
+    withCredentials(usernamePassword[credentialsId: 'git_key',
+                                     usernameVariable: 'GIT_USERNAME',
+                                     passwordVariable: 'GIT_PASSWORD']) {
+        sh"""
+            git clone https://\$GIT_USERNAME:\$GIT_PASSWORD@github.com:Zelgladis/JenkinsTest.git
+        """
+        def versions = []
+        def command = "cat ./JenkinsTest/testUsers.json"
+        def process = command.execute()
+        process.in.eachLine { line ->
+            // Обрабатываем JSON-ответ, чтобы извлечь версии
+            def history = new groovy.json.JsonSlurper().parseText(line)
+            versions = history.collect { it.revision }
+        }
+        echo "$versions"
+    }
 }
