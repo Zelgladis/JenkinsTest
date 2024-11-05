54ad98c39babc70901837be148e2b2da981b688c
Mio <Cep908@gmail.com>
2024-11-05 11:03:44 +0300

    stage fix

--- vars/timeline.groovy
+++ vars/timeline.groovy
@@ -1,10 +1,15 @@
 def call(){
-    withCredentials([usernamePassword(credentialsId: 'git_key', 
-                                                      usernameVariable: 'GIT_USERNAME', 
-                                                      passwordVariable: 'GIT_PASSWORD')]) {
-        sh"""
-            git clone https://\$GIT_USERNAME:\$GIT_PASSWORD@github.com/Zelgladis/JenkinsTest.git
-            ls
-        """
+    stage("Clean"){
+        cleanWs()
+    }
+    stage("ParseTheWOrld"){
+        withCredentials([usernamePassword(credentialsId: 'git_key', 
+                                                        usernameVariable: 'GIT_USERNAME', 
+                                                        passwordVariable: 'GIT_PASSWORD')]) {
+            sh"""
+                git clone https://\$GIT_USERNAME:\$GIT_PASSWORD@github.com/Zelgladis/JenkinsTest.git
+                ls
+            """
+        }
     }
 }
