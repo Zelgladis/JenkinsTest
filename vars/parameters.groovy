20ddedde2ed7f32b85cefab89858f8138aea5448
Mio <Cep908@gmail.com>
2024-11-06 15:47:22 +0300

    ssss

--- vars/parameters.groovy
+++ vars/parameters.groovy
@@ -1,3 +1,13 @@
 def call(){
-    
+    def allParams = []
+    allParams.add(booleanParam(name: "TestBool", defaultValue: false, description: "Test boolean parameter"))
+    allParams.add(string(name: "TestString", defaultValue: "default value", description: "Test string parameter"))
+
+    // Обновляем DynamicChoice с новыми значениями
+    allParams.add([
+        $class: 'ChoiceParameterDefinition', 
+        name: 'DynamicChoice', 
+        choices: ["123", "456", "789"],  // Новые значения
+        description: 'Select a dynamic option'
+    ])
 }
