3d183b5cb28dc04febec7ff3ed771e1a0aad3fc3
Mio <Cep908@gmail.com>
2024-11-06 16:19:04 +0300

    asdasd

--- vars/my_p.groovy
+++ vars/my_p.groovy
@@ -7,37 +7,23 @@ def adasafaffwfqffqwfwf() {
 
     // Собираем все текущие параметры
     for (param in params.keySet()) {
-        if (param == 'VersionRollback') {
-            // Если параметр VersionRollback уже есть, обновляем его
-            updatedParams << [
-                $class: 'ChoiceParameterDefinition',
-                name: 'VersionRollback',
-                choices: timeline(),
-                description: 'Select a version to rollback'
-            ]
-            rollbackExists = true
+        def value = params[param]
+        if (value instanceof Boolean) {
+            updatedParams << [$class: 'BooleanParameterDefinition', name: param, defaultValue: value, description: "Existing boolean parameter: ${param}"]
+        } else if (value instanceof String) {
+            updatedParams << [$class: 'StringParameterDefinition', name: param, defaultValue: value, description: "Existing string parameter: ${param}"]
         } else {
-            // Обрабатываем другие типы параметров
-            def value = params[param]
-            if (value instanceof Boolean) {
-                updatedParams << [$class: 'BooleanParameterDefinition', name: param, defaultValue: value, description: "Existing boolean parameter: ${param}"]
-            } else if (value instanceof String) {
-                updatedParams << [$class: 'StringParameterDefinition', name: param, defaultValue: value, description: "Existing string parameter: ${param}"]
-            } else {
-                // Если есть другие типы, можно добавить доп. обработку
-            }
+            // Если есть другие типы, можно добавить доп. обработку
         }
     }
 
     // Если параметр VersionRollback не существует, добавляем его
-    if (!rollbackExists) {
-        updatedParams << [
-            $class: 'ChoiceParameterDefinition',
-            name: 'VersionRollback',
-            choices: timeline(),
-            description: 'Select a version to rollback'
-        ]
-    }
+    updatedParams << [
+        $class: 'ChoiceParameterDefinition',
+        name: 'VersionRollback',
+        choices: timeline(),
+        description: 'Select a version to rollback'
+    ]
 
     // Устанавливаем параметры с новым и существующими значениями
     properties([
