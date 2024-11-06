6cef0b0502738d0a6cbbe17de5eb5dd3c78ed1de
Mio <Cep908@gmail.com>
2024-11-06 19:24:34 +0300

    asd

--- vars/param_init.groovy
+++ vars/param_init.groovy
@@ -5,10 +5,33 @@ def call(){
         my_params.add(paramen_ret(type: 'bool',name: 'BoolTest', valueBool: true, description: 'Hello world'))
         my_params.add(paramen_ret(type: 'bool',name: 'BoolTest2', description: '2 world'))
 
+        my_params = my_params.findAll { it != null }
+
         properties([
             parameters(my_params)
         ])
     }
+}
+
+def paramen_ret(Map args){
+    def ret
+    pname = args.get('name', 'Null')
+    choices = args.get('choices', 'Null')
+    value = args.get('value', 'Null')
+    valueBool = args.get('valueBool', false)
+    type = args.get('type', 'Null')
+    defValue = args.get('defValue', 'Null')
+    parclass = args.get('parclass', 'Null')
+    description = args.get('description', 'Null')
+
+    switch (type) {
+        case 'bool':
+            ret = booleanParam(name: pname, defaultValue: valueBool, description: description)
+            break
+        default:
+            ret = "Ни одно из условий не подошло"
+    }
+    return ret
 }
 
 
