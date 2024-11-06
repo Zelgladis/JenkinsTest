9076b85a785fc9efe9679e5b134faa701bf15369
Mio <Cep908@gmail.com>
2024-11-06 08:52:15 +0300

    TeSt

--- vars/timeline.groovy
+++ vars/timeline.groovy
@@ -10,7 +10,7 @@ def call(){
                 git clone https://\$GIT_USERNAME:\$GIT_PASSWORD@github.com/Zelgladis/JenkisTestGit.git
             """
             // Тут твоя команда с helm -o json
-            def jsonContent = sh(script: 'cat ./JenkisTestGit/testUsers.json', returnStdout: true).trim()
+            def jsonContent = sh(script: """cat ./JenkisTestGit/testUsers.json""", returnStdout: true).trim()
             // Парсим JSON
             def history = new groovy.json.JsonSlurper().parseText(jsonContent)
             // Извлекаем версии
