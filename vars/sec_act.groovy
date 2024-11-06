d8c464fbc9bc67051835c683dca6484d4f058c10
Mio <Cep908@gmail.com>
2024-11-06 19:43:52 +0300

    asd

--- vars/sec_act.groovy
+++ vars/sec_act.groovy
@@ -1,4 +1,4 @@
 def call(List my_params){
-    my_params.add(paramen_ret(type: 'choice',name: 'VersionRollback', choices: timeline() ,description: 'Select a version to rollback'))
+    my_params.add(param_init.paramen_ret(type: 'choice',name: 'VersionRollback', choices: timeline() ,description: 'Select a version to rollback'))
     param_init.reInit()
 }
