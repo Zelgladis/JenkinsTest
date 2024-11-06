137c4161c0c75fbd1d80c234da5d49ba4cb7c4fa
Mio <Cep908@gmail.com>
2024-11-06 16:15:21 +0300

    asd

--- vars/my_p.groovy
+++ vars/my_p.groovy
@@ -1,4 +1,4 @@
-def addOrUpdateChoice() {
+def addOrUpdateChoice2() {
     // Список для хранения параметров
     def updatedParams = []
 
@@ -42,5 +42,48 @@ def addOrUpdateChoice() {
     // Устанавливаем параметры с новым и существующими значениями
     properties([
         parameters(updatedParams)
+    ])
+}
+
+def addOrUpdateChoice() {
+    // Список для хранения параметров в исходном порядке
+    def orderedParams = []
+    def rollbackUpdated = false
+
+    // Обходим текущие параметры и сохраняем их типы и значения
+    for (param in params.keySet()) {
+        if (param == 'VersionRollback') {
+            // Обновляем существующий параметр VersionRollback
+            orderedParams << [
+                $class: 'ChoiceParameterDefinition',
+                name: 'VersionRollback',
+                choices: timeline(),
+                description: 'Select a version to rollback'
+            ]
+            rollbackUpdated = true
+        } else {
+            def value = params[param]
+            if (value instanceof Boolean) {
+                orderedParams << [$class: 'BooleanParameterDefinition', name: param, defaultValue: value, description: "Existing boolean parameter: ${param}"]
+            } else if (value instanceof String) {
+                orderedParams << [$class: 'StringParameterDefinition', name: param, defaultValue: value, description: "Existing string parameter: ${param}"]
+            }
+            // Добавьте обработку других типов при необходимости
+        }
+    }
+
+    // Если параметр VersionRollback не найден, добавляем его в конец
+    if (!rollbackUpdated) {
+        orderedParams << [
+            $class: 'ChoiceParameterDefinition',
+            name: 'VersionRollback',
+            choices: timeline(),
+            description: 'Select a version to rollback'
+        ]
+    }
+
+    // Устанавливаем параметры, сохраняя исходный порядок
+    properties([
+        parameters(orderedParams)
     ])
 }
