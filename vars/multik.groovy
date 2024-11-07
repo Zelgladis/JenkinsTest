18fe5e7436d1f6926ffacf4c4287c16466af40f0
Mio <Cep908@gmail.com>
2024-11-07 20:50:55 +0300

    asd

--- vars/multik.groovy
+++ vars/multik.groovy
@@ -1,5 +1,7 @@
 def call(){
     mpline.git_pull('main', 'https://github.com/Zelgladis/JenkinsTest.git')
+    def tes = params.isEmpty()
+    echo "$tes"
     if (params.isEmpty()) {
         echo "Set params to []"
         setParams([])
