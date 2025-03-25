41939b9057e1cfd955ce933a1087113bb7c1fea7
Mio <Cep908@gmail.com>
2025-03-25 14:35:39 +0400

    test

--- src/funcutil/ret_params.groovy
+++ src/funcutil/ret_params.groovy
@@ -14,20 +14,20 @@ def ms_build(map yamlData, int __c__){
     if (yamlData.pipelines[__c__].parameters.mvncommand == 'dotnet nupkg') {
         lst_params.add(stringParam('VersionNupkg', '2.0.', 'Версия пакетов'))
     }else {
-        lst_params.add(gitParameter {
-            name('BRANCH_NAME')
-            branch('main')
-            description('Необходимо выбрать ветку для сборки')
-            tagFilter('*')
-            type('BRANCH_TAG')
-            defaultValue('main')
-            quickFilterEnabled(true)
-            branchFilter('origin/(.*)')
-            sortMode('NONE')
-            selectedValue('NONE')
-            useRepository("ssh://git@github.com/${yamlData.pipelines[__c__].parameters.globalSystem}/${yamlData.pipelines[__c__].parameters.GitName}.git")
+        lst_params.add(gitParameter(
+            name: 'BRANCH_NAME',
+            branch: 'main',
+            description: 'Необходимо выбрать ветку для сборки',
+            tagFilter: '*',
+            type: 'BRANCH_TAG',
+            defaultValue: 'main',
+            quickFilterEnabled: true,
+            branchFilter: 'origin/(.*)',
+            sortMode: 'NONE',
+            selectedValue: 'NONE',
+            useRepository: "ssh://git@github.com/${yamlData.pipelines[__c__].parameters.globalSystem}/${yamlData.pipelines[__c__].parameters.GitName}.git"
         )
-    }
+    )
         lst_params.add(booleanParam('Move_Distr', false, 'Перемещение дистрибутива для перекладки в CDL'))
         lst_params.add(booleanParam('Deploy_to_dev', false, 'Установка пакета на DSO'))
         if (yamlData.pipelines[__c__].parameters.Platform == 'OC') {
@@ -38,4 +38,8 @@ def ms_build(map yamlData, int __c__){
     }
     lst_params.add(booleanParam('test', true, 'test'))
     return lst_params
+}
+
+def test(){
+    echo 'worked'
 }
