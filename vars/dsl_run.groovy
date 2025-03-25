ec835466e936d14001790dd175bd8ecf2d50b95a
Mio <Cep908@gmail.com>
2025-03-25 15:37:17 +0400

    test

--- vars/dsl_run.groovy
+++ vars/dsl_run.groovy
@@ -42,7 +42,7 @@ def call(){
         for(int i=0; i < yamlData.pipelines.size(); i++){
             final_content = final_content + "\n" +(jobContent.replace("__c__", "${i}"))
         }
-        printl final_content
+        println final_content
         writeFile(file: "BuildProc_2.groovy", text: "${final_content}")
 //         jobDsl targets: "BuildProc_2.groovy",
 //                 lookupStrategy: 'SEED_JOB',
