074949a569c1f8eb6f9d39f624a3c0425d599aae
Zelgladis <Cep908@gmail.com>
2025-04-28 19:28:53 +0400

    ds

--- vars/dsl_run.groovy
+++ vars/dsl_run.groovy
@@ -52,7 +52,9 @@ def call(){
             pipeGrp[i] = [
                 'pipelines': pipeGrp[i]
             ]
-            //println contentLst[i]
+            println '-----------------------------------------------------------------------------------------------'
+            println contentLst[i]
+            println '-----------------------------------------------------------------------------------------------'
 
             writeFile(file: "BuildProc_2_${i}.groovy", text: "${contentLst[i]}")
             jobDsl targets: "BuildProc_2_${i}.groovy",
